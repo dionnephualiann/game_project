a69cdc523a59f1de9af6398ee878db4615d6e422
Dionne Phua <li.ann86@gmail.com>
2017-08-14 14:02:15 +0800

    refactored files

--- instruction.js
+++ instruction.js
@@ -1,33 +1,32 @@
-var Instruction = function(){
+var Instruction = function () {
 
-//Start Button
-var start =$('<div/>').addClass('start');
+  //Start Button
+  var start = $('<div/>').addClass('start');
 
-var splashPage = $("#background").removeClass("background").addClass("splash")
+  var splashPage = $("#background").removeClass("background").addClass("splash");
 
-//splash page
-
-splashPage.append(start);
+  //splash page
+  splashPage.append(start);
 
 
 // To start game
-	$(start).click(function(){
-		var newGame = new Game();
-		var gameBG= $("#background").removeClass("splash").addClass("background")
-		start.remove();
-	})
-		
-}
+  $(start).click(function () {
+    new Game();
+    $("#background").removeClass("splash").addClass("background");
+    start.remove();
+  })
 
-var audio = document.getElementById("music"); 
+};
+
+var audio = document.getElementById("music");
 
 document.getElementById("mute").addEventListener('click', function (event) {
-  
-	if (audio.paused) {
-  	audio.play();
-} else {
-    audio.pause(); 
-}
+
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
 });
 
 new Instruction();
